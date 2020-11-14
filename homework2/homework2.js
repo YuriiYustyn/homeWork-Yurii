af24071bbbb93eacea02b198e30ab38b5201e74a
YuriiYustyn <gitgithub@gmail.com>
2020-11-14 15:19:04 +0200

    add new file

--- homework2/homework2.js
+++ homework2/homework2.js
@@ -4,24 +4,33 @@ while (Number.isInteger(numberN)===false);
 
 let numberM;
 do {numberM = +prompt('Введіть ціле число M, до якого слід рахувати суму чисел');}
-while (Number.isInteger( numberM)===false);
+while (Number.isInteger( numberM)===false || numberN>numberM );
 
 let miss = confirm('Пропускати парні числа?');
+
 if (miss){
+let sum = 0;
+for (let i = numberN+1; i <=  numberM; i+=2) {
+	sum +=i;
+	}
+	document.writeln(`Сума чисел: ${sum} виходячи з`)
+}
+
+else{
 let sum = 0;
 for (let i = numberN; i <=  numberM; i++) {
 	sum +=i;
 	}
-	console.log(sum);
-}
-else{let sum = 0;
-for (let i = numberN+1; i <=  numberM; i+=2) {
-	sum +=i;
-	}
-	console.log(sum);
+	document.writeln(`Сума чисел: ${sum} <br>виходячи з <br>`)
 }
 
-
+document.writeln(
+`<ul>
+<li> мінімальне число N:  ${numberN} </li> 
+<li>максимальне число М:  ${numberM} </li>
+<li> пропускати парні числа: ${miss}
+</ul>	
+	`);
 
 
  
