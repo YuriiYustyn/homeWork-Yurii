d96b5a43ac842a7434e56c6cfffd66e95dba3a89
YuriiYustyn <gitgithub@gmail.com>
2020-11-19 16:43:29 +0200

    add

--- homework2/main.js
+++ homework2/main.js
@@ -1,34 +1,32 @@
 let numberMin;
 do{ numberMin = +prompt('Введіть ціле число N, від якого рахуватиметься сума чисел');}
-while (Number.isInteger(numberMin)===false);
+while (!Number.isInteger (numberMin));
 
 let numberMax;
 do {numberMax = +prompt('Введіть ціле число M, до якого слід рахувати суму чисел');}
-while (Number.isInteger( numberMax)===false || numberMin>numberMax );
+while (!Number.isInteger( numberMax) || numberMin>numberMax );
 
-const audit = confirm('Пропускати парні числа?');
+const skip = confirm('Пропускати парні числа?');
 
-if (audit){
-let sum = 0;
-for (let i = numberMin+1; i <=  numberMax; i+=2) {
-	sum +=i;
-	}
-	document.writeln(`Сума чисел: ${sum} виходячи з`)
-}
+let sum = 0; 
 
-else{
-let sum = 0;
+
 for (let i = numberMin; i <=  numberMax; i++) {
-	sum +=i;
+	if (skip && i % 2 ===0 ) {
+		continue
 	}
-	document.writeln(`Сума чисел: ${sum} <br>виходячи з <br>`)
-}
+	sum +=i;
+	};
+	
+
+
 
 document.writeln(
 `<ul>
 <li> мінімальне число N:  ${numberMin} </li> 
-<li>максимальне число М:  ${numberMax} </li>
-<li> пропускати парні числа: ${audit}
+<li> максимальне число М:  ${numberMax} </li>
+<li> пропускати парні числа: ${skip}</Li>
+<li> cума чисел: ${sum}</Li>
 </ul>	
 	`);
 
