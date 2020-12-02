c464ea68447a9ce68a6e9bfdbc248fc9c03b8b79
YuriiYustyn <gitgithub@gmail.com>
2020-12-02 20:36:58 +0200

    edited according to the remarks

--- Homework5/home5.js
+++ Homework5/home5.js
@@ -1,8 +1,8 @@
 //повертає масив випадкових цілих чисел
 function getRandomArray(length, max, min) {
-   let randomArray = [];
+   const randomArray = [];
    do {
-      randomArray.push(Math.floor(Math.random() * (max - min + 1)) + min);
+randomArray.push(Math.floor(Math.random() * (max - min + 1)) + min);
    }
    while (randomArray.length < length);
    return randomArray;
@@ -11,10 +11,9 @@ console.log(getRandomArray(15,100,12));
 
 
 //рахує середнє арифметичне всіх переданих в неї аргументів
-let arr = [3, -5, 2, 3, 2, 6, -4, 3, 9, 7, 7, 2]
+const arr = [3, -5, 2, 3, 2, 6, -4, 3, 9, 7, 7, 2]
 function getAverage(array){
-   let getAverage =
-array.filter((item) => {
+   const getAverage = array.filter((item) => {
    return Number.isInteger(item)
 })
    .reduce((acum, number) => {
@@ -26,11 +25,10 @@ console.log(getAverage(arr))
 
 //рахує медіану всіх переданих в неї аргументів
 function getMedian(array) {
-   let sortArr =
-   array.filter((item) => {
+   const sortArr = array.filter((item) => {
       return Number.isInteger(item)
-   }).sort((min, max) => {
-      return min-max
+   }).sort((a, b) => {
+      return a-b
    })
    let median;
    if (sortArr.length % 2 !== 0) {
@@ -45,20 +43,19 @@ console.log(getMedian(arr))
 
 //порахує кількість чисел більших 0
 function countPositiveNumbers(array) {
-   let positiveNumbers = array.filter(function filter(number){
-      return number >0
+   const positiveNumbers = array.filter(function filter(number){
+      return number > 0;
    })
    return positiveNumbers.length
 }
 console.log(countPositiveNumbers(arr))
 
 //відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
-let newArray = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 20, 56, 3, 2]
+const newArray = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 20, 56, 3, 2]
 function getDividedByFive (elements) {
-   let DividedByFive  = elements.filter(function filtered(number) {
+   return elements.filter(function filtered(number) {
       return number %5 ===0
    })
-   return DividedByFive
 }
 console.log(getDividedByFive(newArray));
 
@@ -87,7 +84,7 @@ console.log(divideByThree('comander'))
 document.writeln(`'Функція 1' <br> масив випадкових цілих чисел <br>${getRandomArray(15, 100, 12)} <br>
 'Функція 2'<br>середнє арифметичне від всіх  аргументів<br>${getAverage(arr)}<br>
 'Функція 3'<br> медіана всіх переданих  аргументів<br>${getMedian(arr) }<br>
-'Функція 4'<br>кількість чисел більших 0<br>${countPositiveNumbers(arr)}<br>
+'Функція 4'<br>із чисел ${arr}<br>кількість чисел більших 0<br>${countPositiveNumbers(arr)}<br> 
 'Функція 5'<br> елементи які діляться на ціло на 5<br>${getDividedByFive(newArray)}<br>
 'Функція 6'<br>склади по 3 букви<br>${divideByThree('comander')}<br>
 `)
