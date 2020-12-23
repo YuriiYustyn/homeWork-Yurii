22d34aa8aa66a03791764fe160acbbacab7230d9
YuriiYustyn <gitgithub@gmail.com>
2020-12-23 20:29:23 +0200

    add

---
+++ homework12/home12.js
@@ -0,0 +1,80 @@
+function getInfo(filmNumber) {
+   const peopleTable = document.querySelector('#people-table');
+   peopleTable.innerHTML =
+      `<tr class="people-table-row">
+	        <td class="people-table-cell">Full name</td>
+	        <td class="people-table-cell">Birth year</td>
+	        <td class="people-table-cell">Gender</td>
+	    </tr>`;
+   const filmLink = `https://swapi.dev/api/films/${filmNumber}/`
+   axios.get(filmLink)
+      .then(res => {
+         const characters = res.data.characters;
+
+         characters.map(e => axios.get(e)
+            .then(res => {
+               console.log(res.data);
+               const newRow = document.createElement('tr');
+               newRow.className = "people-table-row"
+               const nameCell = document.createElement('td');
+               nameCell.className = "people-table-cell"
+               nameCell.textContent = res.data.name;
+               const dateCell = document.createElement('td');
+               dateCell.className = "people-table-cell";
+               dateCell.textContent = res.data.birth_year;
+               newRow.append(nameCell);
+               newRow.append(dateCell);
+               const genderCell = document.createElement('td');
+               genderCell.className = "people-table-cell";
+               if (res.data.gender === "n/a" || res.data.gender === "none" || res.data.gender === "hermaphrodite") {
+                  genderCell.innerHTML = `<img src="fa/trans.png" height="50px">`;
+               }
+               else {
+                  genderCell.innerHTML = `<img src="fa/${res.data.gender}.png" height="50px">`;
+               }
+               newRow.append(genderCell);
+               peopleTable.append(newRow);
+            }))
+      });
+}
+console.log("HHHHEEEELLLO")
+document.querySelector('#show-people').addEventListener('submit', (e) => {
+   e.preventDefault();
+   getInfo(document.querySelector('#film').value);
+})
+
+function getPlanets(page) {
+   const planetsTable = document.querySelector('#planets-table');
+   planetsTable.innerHTML = '';
+   axios.get(`https://swapi.dev/api/planets/?page=${page}`)
+      .then(res => {
+         res.data.results.map(e => {
+            const planet = document.createElement('td');
+            planet.className = 'planet';
+            planet.textContent = e.name;
+            planetsTable.append(planet);
+         })
+      })
+}
+let pageCounter = 1;
+getPlanets(pageCounter);
+
+document.querySelector('#planets-page').addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   if (e.submitter.id === 'next' && pageCounter < 6) {
+      pageCounter++;
+      getPlanets(pageCounter);
+   }
+   else if (e.submitter.id === 'previous' && pageCounter >= 2) {
+      pageCounter--;
+      getPlanets(pageCounter);
+   }
+   console.log(pageCounter);
+})
+
+
+
+
+
+
