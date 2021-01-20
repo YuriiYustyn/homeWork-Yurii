0d8596a298482eb0a2533f2ea1afb3538dac695a
YuriiYustyn <gitgithub@gmail.com>
2021-01-20 17:17:16 +0200

    add new homework

---
+++ Homework16/src/App.js
@@ -0,0 +1,14 @@
+
+import  Contacts  from "./components/Contacts.js" 
+import Contracts from './components/Contracts.js'
+
+function App() {
+  return (
+    <div>
+      {/* <Contacts name={"yurii"} name2={"ustyn"} pfone={554554} /> */}
+      <Contracts/>
+    </div>
+  );
+}
+
+export default App;
